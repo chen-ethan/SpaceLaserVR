16ba3f019a9fd5ede033939d5234d623329323eb
ethanchen111@gmail.com <ethanchen111@gmail.com>
2019-03-05 02:00:27 -0800

    added in new assets / particle system

--- Assets/Scripts/AirCan/AirBlow.cs
+++ Assets/Scripts/AirCan/AirBlow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AirBlow : VRTK.VRTK_InteractableObject
 {
@@ -9,12 +10,17 @@ public class AirBlow : VRTK.VRTK_InteractableObject
     public GameObject nozzle;
     public Rigidbody player;
 
+    protected GameObject smoke;
+    protected ParticleSystem particles;
+
+    public float maxSprayPower = .5f;
+    public float minPower = .5f;
     public float blowStrength;
     public float capacity;
 
 
-
-
+    
+    
     public override void StartUsing(VRTK.VRTK_InteractUse currentUsingObject = null)
     {
         base.StartUsing(currentUsingObject);
@@ -22,6 +28,10 @@ public class AirBlow : VRTK.VRTK_InteractableObject
         //gets the cameraRig Object's rigidbody
         //Debug.Log("player - rigidbody = " + currentUsingObject.transform.parent.parent.name);
         player = currentUsingObject.transform.parent.parent.GetComponent<Rigidbody>();
+        smoke = transform.Find("Smoke").gameObject;
+        particles = smoke.GetComponent<ParticleSystem>();
+        particles.Play();
+        //particles.Stop();
     }
 
     public override void StopUsing(VRTK.VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
@@ -29,6 +39,8 @@ public class AirBlow : VRTK.VRTK_InteractableObject
         base.StopUsing(previousUsingObject, resetUsingObjectState);
         controllerEvents = null;
         player = null;
+        particles.Stop();
+
 
     }
 
@@ -40,16 +52,30 @@ public class AirBlow : VRTK.VRTK_InteractableObject
             Blow(power);
             VRTK.VRTK_ControllerHaptics.TriggerHapticPulse(VRTK.VRTK_ControllerReference.GetControllerReference(controllerEvents.gameObject), power * 0.25f, 0.1f, 0.01f);
         }
-        /*else
+        else
         {
             Blow(0f);
         }
-        */
+
     }
 
     protected void Blow(float power){
         //Debug.Log("BS:" + blowStrength + "\t Power: " + power);
-        player.AddForce(nozzle.transform.up * blowStrength * power,ForceMode.Force);
+        if (power > 0)
+        {
+            player.AddForce(-smoke.transform.forward * blowStrength * power, ForceMode.Force);
+            if (particles.isPaused || particles.isStopped)
+            {
+                particles.Play();
+            }
+            ParticleSystem.MainModule mainModule = particles.main;
+            mainModule.startSpeedMultiplier = Math.Max(maxSprayPower * power,minPower);
+        } /*else if(player) 
+        {
+            particles.Stop();
+
+        }*/
+
     }
 
 
---
+++ Assets/Scripts/Asteroid_ClimbableGrabAttatch.cs
@@ -0,0 +1,67 @@
+﻿namespace VRTK.GrabAttachMechanics { 
+
+using System;
+    using UnityEngine;
+
+public class Asteroid_ClimbableGrabAttatch : VRTK_BaseGrabAttach
+{
+    [Header("Climbable Settings", order = 2)]
+
+    [Tooltip("Will respect the grabbed climbing object's rotation if it changes dynamically")]
+    public bool useObjectRotation = false;
+
+    protected override void Initialise()
+    {
+        tracked = false;
+        climbable = true;
+        kinematic = true;
+    }
+
+        public override bool StartGrab(GameObject grabbingObject, GameObject givenGrabbedObject, Rigidbody givenControllerAttachPoint)
+        {
+            if (base.StartGrab(grabbingObject, givenGrabbedObject, givenControllerAttachPoint))
+            {
+                SnapObjectToGrabToController(givenGrabbedObject);
+                grabbedObjectScript.isKinematic = true;
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// The StopGrab method ends the grab of the current Interactable Object and cleans up the state.
+        /// </summary>
+        /// <param name="applyGrabbingObjectVelocity">If `true` will apply the current velocity of the grabbing object to the grabbed object on release.</param>
+        public override void StopGrab(bool applyGrabbingObjectVelocity)
+        {
+            ReleaseObject(applyGrabbingObjectVelocity);
+            base.StopGrab(applyGrabbingObjectVelocity);
+        }
+
+        protected virtual void SetSnappedObjectPosition(GameObject obj)
+        {
+            if (grabbedSnapHandle == null)
+            {
+                obj.transform.position = controllerAttachPoint.transform.position;
+            }
+            else
+            {
+                obj.transform.rotation = controllerAttachPoint.transform.rotation * Quaternion.Inverse(grabbedSnapHandle.transform.localRotation);
+                obj.transform.position = controllerAttachPoint.transform.position - (grabbedSnapHandle.transform.position - obj.transform.position);
+            }
+        }
+
+        protected virtual void SnapObjectToGrabToController(GameObject obj)
+        {
+            if (!precisionGrab)
+            {
+                SetSnappedObjectPosition(obj);
+            }
+            obj.transform.SetParent(controllerAttachPoint.transform);
+        }
+
+    }
+
+}
+
